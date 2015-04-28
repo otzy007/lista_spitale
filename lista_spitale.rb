e6dc5bd0bf0890668fb80718bc4d28e02ff327de
Andrei Gliga <otzy_007@yahoo.com>
2015-04-28 17:52:25 +0300

    ceva ;)

--- lista_spitale.rb
+++ lista_spitale.rb
@@ -7,13 +7,7 @@ require 'dbpedia'
 
 include RDF
 
-Spreadsheet.client_encoding = 'UTF-8'
-book = Spreadsheet.open 'lista-spitalelor-publice-ordonate-alfabetic.xls'
-
-sheet1 = book.worksheet 0
-counter = 0
-
-prefixes = {
+ $prefixes = {
       cotext: "http://opendata.cs.pub.ro/context/",
       foaf: "http://xmlns.com/foaf/0.1/",
       rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
@@ -27,44 +21,147 @@ prefixes = {
       ns1: 'http://opendata.cs.pub.ro/property/'
     }
 
-RDF::N3::Writer.open("lista_spitale.n3", :prefixes => prefixes) do |writer|
-  sheet1.to_enum.drop(1).each do |row|
-    counter += 1
+def import_spitale
+  Spreadsheet.client_encoding = 'UTF-8'
+  book = Spreadsheet.open 'lista-spitalelor-publice-ordonate-alfabetic.xls'
 
-    break if counter == 6
+  sheet1 = book.worksheet 0
+  counter = 0
 
-    hospital_name = ActiveSupport::Inflector.transliterate(row[3].tr("'\"“”", ""))
-    hospital_link = RDF::URI.new("http://opendata.cs.pub.ro/resource/#{hospital_name.gsub(' ', '_')}")
+  RDF::N3::Writer.open("lista_spitale.n3", :prefixes =>  $prefixes) do |writer|
+    sheet1.to_enum.drop(1).each do |row|
+      counter += 1
 
-    # types
-    graph = RDF::Graph.new << [
+      break if counter == 6
+
+      hospital_name = ActiveSupport::Inflector.transliterate(row[3].tr("'\"“”", ""))
+      hospital_link = RDF::URI.new("http://opendata.cs.pub.ro/resource/#{hospital_name.gsub(' ', '_')}")
+
+      # types
+      graph = RDF::Graph.new << [
+          hospital_link,
+          RDF.type,
+          RDF::URI.new("http://dbpedia.org/class/yago/HospitalsInRomania")
+        ]
+
+      graph << [
         hospital_link,
         RDF.type,
-        RDF::URI.new("http://dbpedia.org/class/yago/HospitalsInRomania")
+        RDF::URI.new('http://schema.org/Hospital')
       ]
 
-    graph << [
-      hospital_link,
-      RDF.type,
-      RDF::URI.new('http://schema.org/Hospital')
-    ]
-
-    # label
-    graph << [
-      hospital_link,
-      RDFS.label,
-      hospital_name
-    ]
+      # label
+      graph << [
+        hospital_link,
+        RDFS.label,
+        hospital_name
+      ]
 
 
-    # spital_in_judet
-    graph << [
-      hospital_link,
-      RDF::Vocabulary.new('http://opendata.cs.pub.ro/property/')['spital_in_judet'],
-      RDF::URI.new("http://opendata.cs.pub.ro/resource/#{row[4]}")
-    ]
+      # spital_in_judet
+      # if row[4]
+      #   graph << [
+      #     hospital_link,
+      #     RDF::Vocabulary.new('http://opendata.cs.pub.ro/property/')['spital_in_judet'],
+      #     RDF::URI.new("http://opendata.cs.pub.ro/resource/#{row[4]}_Judet")
+      #   ]
+      # end
 
-    p graph.data.first
-    writer << graph
+      # clasificare
+      if row[2]
+        clasif = row[2].gsub('.', '')
+        clasif_link = RDF::URI.new("http://opendata.cs.pub.ro/resource/Clasificare_Spital_#{clasif.gsub(' ', '_')}")
+
+        classification_type do|writer|
+          writer << RDF::Graph.new << [
+            clasif_link,
+            RDFS.label,
+            clasif
+          ] << [
+            clasif_link,
+            RDF.type,
+            RDF::URI.new("http://opendata.cs.pub.ro/property/clasificare_spital")
+          ]
+        end
+
+        graph << [
+          hospital_link,
+          RDF::Vocabulary.new('http://opendata.cs.pub.ro/property/')['clasificare_spital'],
+          clasif_link
+        ]
+      end
+      # # detalii organizare
+      if row[5]
+        graph << [
+          hospital_link,
+          RDF::Vocabulary.new('http://opendata.cs.pub.ro/property/')['detalii_organizare_spital'],
+          row[5]
+        ]
+      end
+      #
+      # # regiunea de dezvoltare
+      graph << [
+        hospital_link,
+        RDF::Vocabulary.new('http://opendata.cs.pub.ro/property/')['regiune_dezvoltare'],
+        RDF::URI.new("http://opendata.cs.pub.ro/resource/Regiune_Dezvoltare_#{row[1]}")
+      ]
+
+      p graph.data.first
+      writer << graph
+    end
   end
 end
+
+def properties
+  RDF::N3::Writer.open("proprietati_spitale.n3", :prefixes =>  $prefixes) do |writer|
+    writer << RDF::Graph.new << [
+      RDF::URI.new("http://opendata.cs.pub.ro/property/clasificare_spital"),
+      RDFS.label,
+      'clasificare_spital'
+    ] << [
+      RDF::URI.new("http://opendata.cs.pub.ro/property/clasificare_spital"),
+      RDF.type,
+      RDF.property
+    ]
+
+    writer << RDF::Graph.new << [
+      RDF::URI.new("http://opendata.cs.pub.ro/property/spital_in_judet"),
+      RDFS.label,
+      'spital_in_judet'
+    ] << [
+      RDF::URI.new("http://opendata.cs.pub.ro/property/spital_in_judet"),
+      RDF.type,
+      RDF.property
+    ]
+
+    writer << RDF::Graph.new << [
+      RDF::URI.new("http://opendata.cs.pub.ro/property/detalii_organizare_spital"),
+      RDFS.label,
+      'detalii_organizare_spital'
+    ] << [
+      RDF::URI.new("http://opendata.cs.pub.ro/property/detalii_organizare_spital"),
+      RDF.type,
+      RDF.property
+    ]
+
+    writer << RDF::Graph.new << [
+      RDF::URI.new("http://opendata.cs.pub.ro/property/regiune_dezvoltare"),
+      RDFS.label,
+      'regiune_dezvoltare'
+    ] << [
+      RDF::URI.new("http://opendata.cs.pub.ro/property/regiune_dezvoltare"),
+      RDF.type,
+      RDF.property
+    ]
+  end
+end
+
+def classification_type
+  # clasif = nil
+  RDF::N3::Writer.open("clasificari_spitale.n3", :prefixes =>  $prefixes) do |writer|
+    yield writer
+  end
+end
+
+properties
+import_spitale
