45937de0c2a81627f09b1d6ea15603288f15f7b9
Andrei Gliga <otzy_007@yahoo.com>
2015-05-20 08:29:31 +0300

    sameAs and a new type for development_regions

--- regiuni_dezvoltare.rb
+++ regiuni_dezvoltare.rb
@@ -1,17 +1,36 @@
 load 'lista_spitale.rb'
 
-regiuni =
+regions = {
+  'B' => '/Bucharest-Ilfov_(development_region)',
+  'NE' => '/Nord-Est_(development_region)',
+  'NV' => '/Nord-Vest_(development_region)',
+  'SE' => '/Sud-Est_(development_region)',
+  'SV' => '/Sud-Vest_(development_region)',
+  'C' => '/Centru_(development_region)',
+  'S' => '/Sud_(development_region)',
+  'V' => '/Vest_(development_region)'
+}
 
 RDF::N3::Writer.open("regiuni_dezvoltare.n3", :prefixes =>  $prefixes) do |writer|
-  ['B', 'NE', 'NV', 'SE', 'SV', 'C', 'S', 'V'].each do |reg|
+  regions.each do |reg, link|
+    region = RDF::URI.new("http://opendata.cs.pub.ro/resource/Regiune_Dezvoltare_#{reg}")
+
     writer << RDF::Graph.new << [
-      RDF::URI.new("http://opendata.cs.pub.ro/resource/Regiune_Dezvoltare_#{reg}"),
+      region,
       RDFS.label,
       reg
     ] << [
-      RDF::URI.new("http://opendata.cs.pub.ro/resource/Regiune_Dezvoltare_#{reg}"),
+      region,
       RDF.type,
       RDF::URI.new("http://opendata.cs.pub.ro/property/regiune_dezvoltare")
+    ] << [
+      region,
+      RDF.type,
+      RDF::URI.new('http://dbpedia.org/page/Development_regions_of_Romania')
+    ] << [
+      region,
+      OWL.sameAs,
+      RDF::URI.new("http://dbpedia.org/page#{link}")
     ]
   end
 end
